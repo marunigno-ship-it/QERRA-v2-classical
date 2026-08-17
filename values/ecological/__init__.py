5948162ebe9801ce48c3128065516c45f9ece9e5
MARUSSA METOCHARAKI <marunigno@gmail.com>
2026-08-17 13:44:14 +0300

    export rank_animal_startle_avoidance in ecological package init

--- values/ecological/__init__.py
+++ values/ecological/__init__.py
@@ -3,13 +3,18 @@ Suite B: Ecological & Sustainable Companion Suite
 Exposes Suite B ranking functions.
 """
 
-from .ecological_vectors import rank_flora_boundary_protection
+from .ecological_vectors import (
+    rank_flora_boundary_protection,
+    rank_animal_startle_avoidance,
+)
 
 ECOLOGICAL_VECTORS = [
     "flora_boundary_protection",
+    "animal_startle_avoidance",
 ]
 
 __all__ = [
     "rank_flora_boundary_protection",
+    "rank_animal_startle_avoidance",
     "ECOLOGICAL_VECTORS",
 ]
