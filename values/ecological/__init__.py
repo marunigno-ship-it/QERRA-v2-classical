409ba8f3f920be81a12ca24541da224ca56ae07b
MARUSSA METOCHARAKI <marunigno@gmail.com>
2026-08-19 14:02:41 +0300

    export rank_minimal_disturbance_footprint in ecological package init

--- values/ecological/__init__.py
+++ values/ecological/__init__.py
@@ -6,15 +6,18 @@ Exposes Suite B ranking functions.
 from .ecological_vectors import (
     rank_flora_boundary_protection,
     rank_animal_startle_avoidance,
+    rank_minimal_disturbance_footprint,
 )
 
 ECOLOGICAL_VECTORS = [
     "flora_boundary_protection",
     "animal_startle_avoidance",
+    "minimal_disturbance_footprint",
 ]
 
 __all__ = [
     "rank_flora_boundary_protection",
     "rank_animal_startle_avoidance",
+    "rank_minimal_disturbance_footprint",
     "ECOLOGICAL_VECTORS",
 ]
